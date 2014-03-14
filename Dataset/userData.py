6630a885d040878d1aea815b5b8edb0ee72606e8
Eric Chung <chung.yanhon@gmail.com>
2014-03-14 05:16:43 -0700

    added and fixed functions for user table

--- Dataset/userData.py
+++ Dataset/userData.py
@@ -3,8 +3,6 @@ import math
 from random import randint
 from decimal import *
 
-productList = ""
-
 # Note that "code" (2) and "industry" (3) can be tree nodes or leaf nodes.
 userAttributesLevel1 = ["work", "casual"]
 userAttributesLevel2 = ["code", "admin", "gaming", "applications"]
@@ -18,13 +16,7 @@ userAttributesLeafNodes = ["graphic_design", "game_engine", "vector", "animator"
 userAttributesLeafNodesEncoded = ['00000', '00001', '00010', '00011', '00100', '00101', '00110', '00111', '01000', '01001', '01010', '01011', '01100', '01101', '01110', '01111', '1000', '1001', '1010', '1011', '11000', '11001', '11010', '11011', '11100', '11101', '11110', '11111']
 
 # Dictionary mapping that represents our staticly generated decision tree.
-userAttributesEncodedDictionary = {'0' : "work", '1' : "casual", '00' : "code", '01' : "admin", '10' : "gaming", '11' : "applications", '000' : "graphic_intensive", '001' : "data_intensive", '010' : "advance_usage", '011' : "simple_usage", '100' : "hardcore", '101' : "softcore", '110' : "browsing", '111' : "software", '0000' : "game_dev", '0001' : "artist", '0010' : "scientist", '0011' : "engineer", '0100' : "comm", '0101' : "data", '0110' : "word_processing", '0111' : "pos", '1000' : "repetitive", '1001' : "strategic", '1010' : "real_time", '1011' : "casino_games", '1100' : "media", '1101' : "website", '1110' : "writing", '1111' : "designing", '00000' : "graphic_design", '00001' : "game_engine", '00010' : "vector", '00011' : "animator", '00100' : "research", '00101' : "industry", '00110' : "school", '00111' : "industry", '01000' : "high_end_user", '01001' : "low_end_user", '01010' : "operator", '01011' : "system_admin", '01000' : "student", '01001' : "industry", '01010' : "small_business", '01011' : "large_business", '11000' : "pictures", '11001' : "video", '11010' : "online_gaming", '11011' : "social", '11100' : "long_form", '11101' : "short_form", '11110' : "code", '11111' : "art"}
-
-writeFile = open("userData.csv", mode="w")
-writeFile.write("UserId,ProductId,Rating,UserAttributes\n")
-
-# userCounter also serves as UserId.
-userCounter = 0
+userAttributesEncodedDictionary = {'0' : "work", '1' : "casual", '00' : "code", '01' : "admin", '10' : "gaming", '11' : "applications", '000' : "graphic_intensive", '001' : "data_intensive", '010' : "advance_usage", '011' : "simple_usage", '100' : "hardcore", '101' : "softcore", '110' : "browsing", '111' : "software", '0000' : "game_dev", '0001' : "artist", '0010' : "scientist", '0011' : "engineer", '0100' : "comm", '0101' : "data", '0110' : "word_processing", '0111' : "pos", '1000' : "repetitive", '1001' : "strategic", '1010' : "real_time", '1011' : "casino_games", '1100' : "media", '1101' : "website", '1110' : "writing", '1111' : "designing", '00000' : "graphic_design", '00001' : "game_engine", '00010' : "vector", '00011' : "animator", '00100' : "research", '00101' : "industry", '00110' : "school", '00111' : "industry", '01000' : "high_end_user", '01001' : "low_end_user", '01010' : "operator", '01011' : "system_admin", '01100' : "student", '01101' : "industry", '01110' : "small_business", '01111' : "large_business", '11000' : "pictures", '11001' : "video", '11010' : "online_gaming", '11011' : "social", '11100' : "long_form", '11101' : "short_form", '11110' : "code", '11111' : "art"}
 
 # Probability user inputs an uncommon user attribute and chooses accurate user preferences, respectively. Crossover probability calculated by 0.5 * x = 0.05.
 crossoverProbability = 5
@@ -35,35 +27,82 @@ numLevels = 5
 
 numErrorPrint = 0
 
-def swapBit(productId, index):
-	if productId[index] == '1':
-		return productId[:index] + '0' + productId[index+1:]
+# Helper function for getRandUserAttributes.
+def swapBit(association, index):
+	# Special case where leaf nodes are not present in this part of the subtree.
+	if association[:2] != '10':
+		if association[index] == '1':
+			newAssociation = association[:index] + '0' + association[index+1:]
+			if newAssociation[:2] != '10':
+				return newAssociation
+			else:
+				return association
+		else:
+			newAssociation = association[:index] + '1' + association[index+1:]
+			if newAssociation[:2] != '10':
+				return newAssociation
+			else:
+				return association
 	else:
-		return productId[:index] + '1' + productId[index+1:]
+		if index < 4:
+			if association[index] == '1':
+				return association[:index] + '0' + association[index+1:]
+			else:
+				return association[:index] + '1' + association[index+1:]
+		else:
+			return association
+
+# Returns an encoding that can be mapped to a list of products per leaf node that may satisfy query.
+def getRandProductAssociation():
+	return userAttributesLeafNodesEncoded[randint(0, 27)]
+
+# Randomly selects a product based off encoding.
+def getRandProduct(association):
+	return randint(1, 200)
 
 # 28 total leaf nodes.
-def getRandUserAttributesForProduct(productId):
+def getRandUserAttributes(association):
 	global numErrorPrint
 	attributes = []
-	currentMaxLevel = len(productId)
-	for index in range(len(productId)):
-		node = productId[:index]
+	currentMaxLevel = len(association)
+
+	for index in range(len(association)):
+		node = association[:index+1]
 		if randint(1, 100) <= correctPathProbability:
 			attributes.append(node)
 
 	while currentMaxLevel > 0 and len(attributes) < maxNumUserAttributes:
 		# User has a 5% chance of incorrectly inputting a user attribute to the product.
 		if randint(1, 100) <= crossoverProbability and randint(1, 100) <= incorrectBasePathProbability/math.pow(2, numLevels - currentMaxLevel):
-			attributes.append(swapBit(productId, currentMaxLevel-1))
+			newAssociation = swapBit(association, currentMaxLevel-1)
+			if newAssociation not in attributes:
+				attributes.append(newAssociation)
 			numErrorPrint += 1
 		currentMaxLevel -= 1
-
+	
 	return attributes
 
-def getRandProduct():
-	return userAttributesLeafNodesEncoded[randint(0, 27)]
+# Create 1000 users.
+def createUserTable():
+	writeFile = open("userData.csv", mode="w")
+	writeFile.write("UserId,ProductId,Rating,UserAttributes\n")
 
-for i in range(1000):
-	getRandUserAttributesForProduct(getRandProduct())
+	for user in range(1000):
+		numRatings = randint(1, 20)
+		for row in range(numRatings):
+			association = getRandProductAssociation()
+			product = getRandProduct(association)
+			encodedAttributes = getRandUserAttributes(association)
+			attributes = ""
+			for encoding in encodedAttributes:
+				attributes += userAttributesEncodedDictionary[encoding] + " "
 
-print numErrorPrint
+			rating = (randint(1, 10))/2.0
+			rating = round(rating, 1)
+
+			# For now, we only assume each association (leaf node) represents only one product.
+			writeFile.write(str(user) + ", " + str(product) + ", " + str(rating) + ", " + attributes + "\n")
+
+	writeFile.close()
+
+createUserTable()
