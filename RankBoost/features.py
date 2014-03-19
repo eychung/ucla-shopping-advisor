a40b6a2be265d2b9bcae34f1575ba7bcbae169b8
Eric <eric@eric-VirtualBox.(none)>
2014-03-19 16:26:44 -0700

    fixed features and cleaned up repo

--- RankBoost/features.py
+++ RankBoost/features.py
@@ -4,33 +4,22 @@ import itertools
 import random
 import sys
 
-pal = ["DiscreteGPU", "LargeHDD", "IntegratedWebCam", "IntegratedMic", "HighDPI", "HighBattery", "Rugged", "BackLitKeyboard", "LightWeight", "HighRAM", "SDCard", "OpticalDrive"]
+dataPath = "../Dataset/"
+trainPath = "train/"
+testPath = "test/"
 
-# the ones that we came up with for the leaf nodes (24 base cases)
-baseProductSet = { 0 : pal[0] + " " + pal[1] + " " + pal[4] + " " + pal[7] + " " + pal[9],
-1 : pal[0] + " " + pal[1] + " " + pal[4] + " " + pal[7] + " " + pal[9] + " " + pal[11],
-2 : pal[0] + " " + pal[1] + " " + pal[4] + " " + pal[9] + " " + pal[10],
-3 : pal[0] + " " + pal[1] + " " + pal[2] + " " + pal[4] + " " + pal[9],
-4 : pal[1] + " " + pal[2] + " " + pal[5] + " " + pal[9] + " " + pal[11],
-5 : pal[1] + " " + pal[5] + " " + pal[6] + " " + pal[9] + " " + pal[11],
-6 : pal[1] + " " + pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[9],
-7 : pal[1] + " " + pal[2] + " " + pal[3] + " " + pal[6] + " " + pal[9],
-8 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[7] + " " + pal[8] + " " + pal[11],
-9 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[7] + " " + pal[8],
-10 : pal[1] + " " + pal[3] + " " + pal[7] + " " + pal[9] + " " + pal[11],
-11 : pal[1] + " " + pal[2] + " " + pal[3] + " " + pal[6] + " " + pal[7] + " " + pal[9],
-12 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[6] + " " + pal[7],
-13 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[6] + " " + pal[9],
-14 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[6] + " " + pal[7] + " " + pal[9],
-15 : pal[0] + " " + pal[1] + " " + pal[4] + " " + pal[6] + " " + pal[7],
-16 : pal[0] + " " + pal[1] + " " + pal[4] + " " + pal[6] + " " + pal[7] + " " + pal[9],
-17 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[8] + " " + pal[11],
-18 : pal[2] + " " + pal[3] + " " + pal[5] + " " + pal[6] + " " + pal[8],
-19 : pal[1] + " " + pal[2] + " " + pal[5] + " " + pal[9] + " " + pal[10],
-20 : pal[1] + " " + pal[2] + " " + pal[5] + " " + pal[9] + " " + pal[10] + " " + pal[11],
-21 : pal[3] + " " + pal[4] + " " + pal[5] + " " + pal[6] + " " + pal[7] + " " + pal[8],
-22 : pal[3] + " " + pal[4] + " " + pal[5] + " " + pal[6] + " " + pal[8],
-23 : pal[5] + " " + pal[7] + " " + pal[8] + " " + pal[9] + " " + pal[10] + " " + pal[11] }
+userTableFile = "userData.csv"
+productTableFile = "products.txt"
+trainFile = "Train.csv"
+testFile = "Test.csv"
+
+# Note that "code" (2) and "industry" (3) can be tree nodes or leaf nodes.
+userAttributesLevel1 = ["work", "casual"]
+userAttributesLevel2 = ["code1", "admin", "gaming", "applications"]
+userAttributesLevel3 = ["graphic_intensive", "data_intensive", "advance_usage", "simple_usage", "hardcore", "softcore", "browsing", "software"]
+userAttributesLevel4 = ["game_dev", "artist", "scientist", "engineer", "comm", "data", "word_processing", "pos", "repetitive", "strategic", "real_time", "casino_games", "media", "website", "writing", "designing"]
+userAttributesLevel5 = ["graphic_design", "game_engine", "vector", "animator", "research", "industry1", "school", "industry2", "high_end_user", "low_end_user", "operator", "system_admin", "student", "industry3", "small_business", "large_business", "pictures", "video", "online_gaming", "social", "long_form", "short_form", "code2", "art"]
+userAttributes = userAttributesLevel1 + userAttributesLevel2 + userAttributesLevel3 + userAttributesLevel4 + userAttributesLevel5
 
 class User():
 	def __init__(self):
@@ -51,19 +40,20 @@ def loadUsersProducts():
 	users = {}
 	products = {}
 	
-	with open('../Dataset/userData.csv') as csvfile:
+	with open(dataPath + userTableFile) as csvfile:
 		reader = csv.reader(csvfile)
 		reader.next()
 		for userId, productId, rating, userAttributes in reader:
 			userId, productId = int(userId), int(productId)
 			if userId not in users:
 				users[userId] = User()
-			users[userId].reviews = Review()
-			users[userId].reviews.product = productId
-			users[userId].reviews.rating = rating
-			users[userId].reviews.userAttributes.append(userAttributes)
+			userReview = Review()
+			userReview.product = productId
+			userReview.rating = rating
+			userReview.userAttributes.append(userAttributes)
+			users[userId].reviews.append(userReview)
 
-	with open('../Dataset/products.txt') as csvfile:
+	with open(dataPath + productTableFile) as csvfile:
 		reader = csv.reader(csvfile)
 		reader.next()
 		for productId, base, technicalAttributes in reader:
@@ -74,68 +64,54 @@ def loadUsersProducts():
 	
 	return users, products
 
+def saveFeature(feature, name, mode):
+	path = ""
+	if mode == 'train':
+		path = trainPath
+	elif mode == 'test':
+		path = testPath
+	filename = path + name + '.' + mode
+	print 'saving feature to', filename, '...'
+	cPickle.dump(feature, open(filename, 'wb'))
+
 def csvGenerator(mode):
 	if mode == 'train':
-		with open("mylabels.train") as csvfile:
+		with open(dataPath + trainFile) as csvfile:
 			reader = csv.reader(csvfile)
-			reader.next() # skip header
-			for node, techAttrPresenceEncoding in reader:
-				node = int(node)
-				techAttrPresenceEncoding = [int(num) for num in techAttrPresenceEncoding.split(' ')]
-				yield node, techAttrPresenceEncoding 
+			reader.next()
+			for productId, rating, correctUserAttributes, incorrectUserAttributes in reader:
+				yield productId, correctUserAttributes, incorrectUserAttributes
 
 	elif mode == 'test':
-		with open('mylabels.train') as csvfile: # stub
+		with open(dataPath + testFile) as csvfile:
 			reader = csv.reader(csvfile)
-			reader.next() # skip header
-			for node, techAttrPresenceEncoding in reader:
-				node = int(node)
-				techAttrPresenceEncoding = [int(num) for num in techAttrPresenceEncoding.split(' ')]
-				yield node, techAttrPresenceEncoding 
+			reader.next()
+			for productId, rating, userAttributes in reader:
+				yield productId, userAttributes
 
 	else:
 		print 'mode must be "train" or "test"'
 		raise ValueError
 
-def saveFeature(feature, name, mode):
-	filename = name + '.' + mode
-	print 'saving feature to', filename, '...'
-	cPickle.dump(feature, open(filename, 'wb'))
-
-# Helper function that puts all training labels for the 54 nodes into one .csv file.
-def createLabelsFile(path='../Dataset/train/'):
-	numNodes = 54
-
-	writeFile = open("mylabels.train", mode="w")
-	writeFile.write("Node,TechnicalAttributes" + "\n")
-	for i in range(numNodes):
-		writeFile.write(str(i+1) + ",")
-		f = open(path + 'train.labels.node' + str(i+1), 'r')
-		savedLine = ""
-		for line in f:
-			savedLine += line.strip() + " "
-		writeFile.write(savedLine.strip())
-		writeFile.write("\n")
-	writeFile.close()
-
-# Used to create the labels.train file that holds a 2D array, with the rows representing the 54 nodes in the decision tree and the columns contain the presence or absence of each of the 24 base products.
 def labels(mode='train'):
 	labels = []
-
 	if mode == 'train':
-		with open("mylabels.train") as csvfile:
+		with open(dataPath + trainFile) as csvfile:
 			reader = csv.reader(csvfile)
 			reader.next()
-			for node, techAttrPresenceEncoding in reader:
+			for productId, rating, correctUserAttributes, incorrectUserAttributes in reader:
 				mylabels = []
-				techAttrPresenceEncoding = [int(num) for num in techAttrPresenceEncoding.split(' ')]
-				for tapenum in techAttrPresenceEncoding:
-					mylabels.append(tapenum)
+				correctUserAttributes = [ua for ua in correctUserAttributes.split(' ')]
+				incorrectUserAttributes = [ua for ua in incorrectUserAttributes.split(' ')]
+				for cua in correctUserAttributes:
+					mylabels.append(1) # 1 = correct
+				for iua in incorrectUserAttributes:
+					mylabels.append(0) # 0 - incorrect
 				labels.append(mylabels)
 	
 	elif mode == 'test':
-		for node, techAttrPresenceEncoding in csvGenerator(mode=mode):
-			labels.append([node, techAttrPresenceEncoding])
+		for productId, userAttributes in csvGenerator(mode=mode):
+			labels.append([productId, userAttributes])
 	
 	else:
 		print 'mode must be "train" or "test"'
@@ -143,19 +119,27 @@ def labels(mode='train'):
 
 	saveFeature(labels, name='labels', mode=mode)
 
-def createFeatureFiles(mode='train'):
-	for technicalAttribute in pal:
+def createFeatureFiles(users, products, mode='train'):
+	# Create a file for each user attribute (node in decision tree).
+	for userAttribute in userAttributes:
 		features = []
-		for baseProduct in baseProductSet:
-			if str(technicalAttribute) in baseProductSet[baseProduct]:
-				features.append(baseProduct)
-		saveFeature(features, name=technicalAttribute, mode=mode)
+		# Iterate through each user.
+		for userId in users:
+			myfeatures = []
+			# Iterate through each product (that has been reviewed by user).
+			for productReview in users[userId].reviews:
+				if userAttribute in productReview.userAttributes:
+					myfeatures.append(1) # 1 = present
+				else:
+					myfeatures.append(0) # 0 = not present
+			features.append(myfeatures)
+
+		saveFeature(features, name=userAttribute, mode=mode)
 
 if __name__ == '__main__':
 	users, products = loadUsersProducts()
 	
-	createLabelsFile()
 	for mode in ['train', 'test']:
 		labels(mode=mode)
 
-	createFeatureFiles()
+	createFeatureFiles(users, products)
--- RankBoost/train.py
+++ RankBoost/train.py
@@ -6,201 +6,89 @@ from sklearn.cross_validation import ShuffleSplit, KFold
 from rankboost import BipartiteRankBoost
 import random
 from sklearn.externals.joblib import Parallel, delayed
+import sklearn
+import features as feat
 
+# features: from loadFeatures
+# labels: labels.train
+def crossValidation(labels, features, classifier, train_products, test_products, pairwise=False):
+	assert pairwise == False
 
-def crossValidation(labels, features, classifier, train_authors, test_authors, pairwise=False):
+	y_train = [val for idx in train_products for val in labels[idx]]
+	X_train = [val for idx in train_products for val in features[idx]]
+	classifier = classifier.fit(X_train, y_train)
 
-    assert pairwise == False
+	X_test = [val for idx in test_products for val in features[idx]]
+	P_test = classifier.predict_proba(X_test)[:,1]
 
-    X_train = [val for idx in train_authors for val in features[idx]]
-    y_train = [val for idx in train_authors for val in labels[idx]]
-    classifier = classifier.fit(X_train, y_train)
+	myscore = 0.0
+	flatidx = 0
+	for idx in test_products:
+		myfeatures, mylabels = features[idx], labels[idx]
 
-    X_test = [val for idx in test_authors for val in features[idx]]
-    P_test = classifier.predict_proba(X_test)[:,1]
-
-    myscore = 0.0
-    flatidx = 0
-    for idx in test_authors:
-        myfeatures, mylabels = features[idx], labels[idx]
-
-        npapers = len(mylabels)
-        ranking = P_test[flatidx:flatidx+npapers].argsort()[::-1]
-        flatidx += npapers
+		ntechattr = len(mylabels)
+		ranking = P_test[flatidx:flatidx+ntechattr].argsort()[::-1]
+		flatidx += ntechattr
 
         ranked_labels = [mylabels[rank] for rank in ranking]
 
-        myscore += scoreAuthor(ranked_labels)
+        myscore += scoreProduct(ranked_labels)
 
-    return myscore/len(test_authors)
+	return myscore/len(test_products)
 
-def shuffleCrossValidation(labels, features, classifier,
-                           n_iter=5, test_size=0.25, random_state=1,
-                           verbose=0, pairwise=False):
-
-    ss = ShuffleSplit(len(labels), n_iter=n_iter,
-                      test_size=test_size, random_state=random_state)
-
-    score = []
-    for train_authors, test_authors in ss:
-
-        if not pairwise:
-            X_train = [val for idx in train_authors for val in features[idx]]
-            y_train = [val for idx in train_authors for val in labels[idx]]
-        else:
-            X_train = [features[idx] for idx in train_authors]
-            y_train = [labels[idx] for idx in train_authors]
-            y_train, X_train = pairwise_transform(y_train, X_train)
-            def classifierComp(x1, x2):
-                xtrans = x2 + x1 + [x2 - x1 for (x1, x2) in zip(x1,x2)]
-                return int(classifier.predict(xtrans)[0])
-
-        classifier = classifier.fit(X_train, y_train)
-
-        if not pairwise:
-            # predict everything at once
-            X_test = [val for idx in test_authors for val in features[idx]]
-            P_test = classifier.predict_proba(X_test)[:,1]
-
-        myscore = 0.0
-        flatidx = 0
-        for idx in test_authors:
-            myfeatures, mylabels = features[idx], labels[idx]
-
-            if pairwise:
-                c = zip(myfeatures, mylabels)
-                random.shuffle(c)
-                myfeatures = [e[0] for e in c]
-                mylabels = [e[1] for e in c]
-
-                ranking = sorted(range(len(myfeatures)),
-                                 key=myfeatures.__getitem__,
-                                 cmp=classifierComp)
-            else:
-                npapers = len(mylabels)
-                ranking = P_test[flatidx:flatidx+npapers].argsort()[::-1]
-                flatidx += npapers
-
-            ranked_labels = [mylabels[rank] for rank in ranking]
-
-            myscore += scoreAuthor(ranked_labels)
-
-        score.append(myscore/len(test_authors)) # MAP
-        if verbose > 0:
-            print 'score:', myscore/len(test_authors)
-    
-    score = np.array(score)
-    print 'score mean, std, mean-std:', score.mean(), score.std(), score.mean() - score.std()
-
-def trainAndPredict(trainlabels, trainfeatures,
-                    testlabels, testfeatures, classifier, pairwise=False):
-
-    if not pairwise:
-        X_train = [paperfeature for authorfeatures in trainfeatures
-                   for paperfeature in authorfeatures]
-        y_train = [paperlabel for authorlabels in trainlabels
-                   for paperlabel in authorlabels]
-    else:
-        X_train = [authorfeatures for authorfeatures in trainfeatures]
-        y_train = [authorlabels for authorlabels in trainlabels]
-        y_train, X_train = pairwise_transform(y_train, X_train)
-        def classifierComp(x1, x2):
-            xtrans = x2 + x1 + [x2 - x1 for (x1, x2) in zip(x1,x2)]
-            return int(classifier.predict(xtrans)[0])
-
-    classifier.fit(X_train, y_train)
-
-    if not pairwise:
-        X_test = [paperfeature for authorfeatures in testfeatures
-                  for paperfeature in authorfeatures]
-        P_test = classifier.predict_proba(X_test)[:,1]
-
-    flatidx = 0
-    ranked_papers = []
-    for idx, labels in enumerate(testlabels):
-        author, papers = labels
-
-        if pairwise:
-            ranking = sorted(range(len(testfeatures[idx])),
-                             key=testfeatures[idx].__getitem__,
-                             cmp=classifierComp)
-        else:
-            npapers = len(papers)
-            ranking = P_test[flatidx:flatidx+npapers].argsort()[::-1]
-            flatidx += npapers
-
-        ranked_papers.append([author, [papers[rank] for rank in ranking]])
-
-    with open('submission.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for author, papers in ranked_papers:
-            writer.writerow([author, " ".join([str(pid) for pid in papers])])
-
-def pairwise_transform(labels, features):
-
-    new_features = []
-    new_labels = []
-    for label, feature in zip(labels, features):
-
-        nconfirmed = sum(label)
-        for iconf, idel in itertools.product(range(nconfirmed), range(nconfirmed, len(label))):
-            assert label[iconf] == 1 and label[idel] == 0
-            new_features.append(feature[iconf] + feature[idel] + [feat1 - feat2 for (feat1, feat2) in zip(feature[iconf], feature[idel])])
-            new_labels.append(1)
-            new_features.append(feature[idel] + feature[iconf] + [feat2 - feat1 for (feat1, feat2) in zip(feature[iconf], feature[idel])])
-            new_labels.append(-1)
-
-    return new_labels, new_features
-
+# Goes through the each feature (technical attribute) in the list.
 def loadFeatures(namelist, mode):
-    featurelist = []
-    for name in namelist:
-        filename = name + '.' + mode
-        featurelist.append(cPickle.load(open(filename, 'rb')))
-    features = []
-    for feats in zip(*featurelist):
-        features.append([list(tup) for tup in zip(*feats)])
+	featurelist = []
+	for name in namelist:
+		filename = name + '.' + mode
+		featurelist.append(cPickle.load(open(filename, 'rb')))
+	features = []
+	for feats in zip(*featurelist):
+		features.append([list(tup) for tup in zip(*feats)])
+	return features
 
-    return features
+# Computes average precision for ranked labels of an author's papers.
+def scoreProduct(ranked_labels):
+	score = 0.0
+	confirmedCount = 0
+	for idx, label in enumerate(ranked_labels):
+		if label is 1:
+			confirmedCount += 1
+			score += float(confirmedCount)/float(idx+1)
+	score /= ranked_labels.count(1)
 
-def scoreAuthor(ranked_labels):
-    # computes average precision for ranked labels of an author's papers
+	return score
 
-    score = 0.0
-    confirmedCount = 0
-    for idx, label in enumerate(ranked_labels):
-        if label is 1:
-            confirmedCount += 1
-            score += float(confirmedCount)/float(idx+1)
-    score /= ranked_labels.count(1)
-
-    return score
-
+# train_products are the 24 base products acquired from cv_products (using trainlabels)
 if __name__ == '__main__':
 	classifier = BipartiteRankBoost(n_estimators=50, verbose=1)
 
-    
-	feature_list = ["DiscreteGPU", "LargeHDD", "IntegratedWebCam", "IntegratedMic", "HighDPI", "HighBattery", "Rugged", "BackLitKeyboard", "LightWeight", "HighRAM", "SDCard", "OpticalDrive"]
+	feature_list = feat.userAttributes
 
 	trainfeatures = loadFeatures(feature_list, mode='train')
 	trainlabels = cPickle.load(open('labels.train', 'rb'))
 
 	print "train features and train labels set" 
         
-	cv_authors = KFold(len(trainlabels), n_folds=5, indices=True, shuffle=True, random_state=1)
+	cv_products = KFold(len(trainlabels), n_folds=5, indices=True, shuffle=True, random_state=1)
+	for train_products, test_products in cv_products:
+		print train_products
     
-	score = Parallel(n_jobs=-1)(delayed(crossValidation)(trainlabels, trainfeatures, classifier, train_authors, test_authors, pairwise=False) for train_authors, test_authors in cv_authors)
+	score = Parallel(n_jobs=-1)(delayed(crossValidation)(trainlabels, trainfeatures, classifier, train_products, test_products, pairwise=False) for train_products, test_products in cv_products)
 
 	score = np.array(score)
 	print 'score mean, std, mean-std:', score.mean(), score.std(), score.mean() - score.std()
-    
-    #testfeatures = loadFeatures(feature_list, mode='test')
-    #testlabels = cPickle.load(open('labels.test', 'rb')) 
-
-    #trainAndPredict(trainlabels, trainfeatures, testlabels, testfeatures, classifier, pairwise=False)
 
 
 
 
-    #shuffleCrossValidation(trainlabels, trainfeatures, classifier, n_iter=5, verbose=0, pairwise=False)
+
+
+
+
+
+
+
+
+
 
